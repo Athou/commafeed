8b1022a4e1e2314261bd9e9f29244d6fb9817bb5
Athou <jeremiepanzer@gmail.com>
2013-03-22 23:04:02 +0100

    fix table name

--- src/main/java/com/commafeed/model/UserSettings.java
+++ src/main/java/com/commafeed/model/UserSettings.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "USERS")
+@Table(name = "USERSETTINGS")
 @SuppressWarnings("serial")
 public class UserSettings implements Serializable {
 

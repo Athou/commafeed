7143b886ee629f65d6ebdbc2e1dbc20471bff9ee
Athou <jeremiepanzer@gmail.com>
2013-04-12 15:50:13 +0200

    junit tests setup

---
+++ src/test/java/com/commafeed/backend/SampleTest.java
@@ -0,0 +1,12 @@
+package com.commafeed.backend;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void init() {
+		Assert.assertEquals(2, 2);
+	}
+}

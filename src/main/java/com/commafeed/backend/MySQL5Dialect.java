c3a271fcd000086f7dedb5b87826a5741bae9ef7
Athou <jeremiepanzer@gmail.com>
2013-04-21 18:59:38 +0200

    not needed anymore as we now strip utf8 4-byte characters

--- src/main/java/com/commafeed/backend/MySQL5Dialect.java
+++ src/main/java/com/commafeed/backend/MySQL5Dialect.java
@@ -5,6 +5,6 @@ import org.hibernate.dialect.MySQL5InnoDBDialect;
 public class MySQL5Dialect extends MySQL5InnoDBDialect {
 
 	public String getTableTypeString() {
-		return " ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
+		return " ENGINE=InnoDB DEFAULT CHARSET=utf8";
 	}
 }

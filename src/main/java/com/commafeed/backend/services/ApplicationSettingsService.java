aef82fe6906d3cd5c51b199c9500ae975e294721
Athou <jeremiepanzer@gmail.com>
2013-04-30 07:11:57 +0200

    store a copy of the settings in memory

--- src/main/java/com/commafeed/backend/services/ApplicationSettingsService.java
+++ src/main/java/com/commafeed/backend/services/ApplicationSettingsService.java
@@ -1,24 +1,31 @@
 package com.commafeed.backend.services;
 
-import javax.ejb.Stateless;
+import javax.ejb.Singleton;
 import javax.inject.Inject;
 
 import com.commafeed.backend.dao.ApplicationSettingsDAO;
 import com.commafeed.backend.model.ApplicationSettings;
 import com.google.common.collect.Iterables;
 
-@Stateless
+@Singleton
 public class ApplicationSettingsService {
 
 	@Inject
 	ApplicationSettingsDAO applicationSettingsDAO;
 
+	private ApplicationSettings settings;
+
 	public void save(ApplicationSettings settings) {
+		this.settings = settings;
 		applicationSettingsDAO.saveOrUpdate(settings);
 	}
 
 	public ApplicationSettings get() {
-		return Iterables.getFirst(applicationSettingsDAO.findAll(), null);
+		if (settings == null) {
+			settings = Iterables.getFirst(applicationSettingsDAO.findAll(),
+					null);
+		}
+		return settings;
 	}
 
 }

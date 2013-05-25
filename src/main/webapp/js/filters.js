89c4f999f09f22b8f3e338d8fa9dc9bb8077315b
Athou <jeremiepanzer@gmail.com>
2013-05-25 15:46:42 +0200

    display relative time if difference is less than 24 hours, even if days are not the same

--- src/main/webapp/js/filters.js
+++ src/main/webapp/js/filters.js
@@ -9,7 +9,7 @@ module.filter('entryDate', function() {
 		var d = moment(timestamp);
 		var now = moment();
 		var formatted;
-		if (d.date() === now.date() && Math.abs(d.diff(now)) < 86400000) {
+		if (Math.abs(d.diff(now)) < 86400000) {
 			formatted = d.fromNow();
 		} else {
 			formatted = d.format('YYYY-MM-DD HH:mm');

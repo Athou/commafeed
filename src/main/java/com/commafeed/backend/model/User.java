05f80d6765d6cb98406edf2e7fa7dca2fe53cf20
Athou <jeremiepanzer@gmail.com>
2013-06-17 08:50:08 +0200

    unused import

--- src/main/java/com/commafeed/backend/model/User.java
+++ src/main/java/com/commafeed/backend/model/User.java
@@ -7,7 +7,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;

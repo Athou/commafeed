7e2ebf2dfceab334dc2ea42f0858765a19a4f507
Athou <jeremiepanzer@gmail.com>
2013-07-04 10:21:00 +0200

    don't filter on having subscriptions as the query is too long to execute

--- src/main/java/com/commafeed/backend/dao/FeedDAO.java
+++ src/main/java/com/commafeed/backend/dao/FeedDAO.java
@@ -27,9 +27,6 @@ public class FeedDAO extends GenericDAO<Feed> {
 
 	private List<Predicate> getUpdatablePredicates(Root<Feed> root, Date threshold) {
 
-		Predicate hasSubscriptions = builder.isNotEmpty(root
-				.get(Feed_.subscriptions));
-
 		Predicate neverUpdated = builder.isNull(root.get(Feed_.lastUpdated));
 		Predicate updatedBeforeThreshold = builder.lessThan(
 				root.get(Feed_.lastUpdated), threshold);
@@ -39,7 +36,7 @@ public class FeedDAO extends GenericDAO<Feed> {
 		Predicate disabledDateIsInPast = builder.lessThan(
 				root.get(Feed_.disabledUntil), new Date());
 
-		return Lists.newArrayList(hasSubscriptions,
+		return Lists.newArrayList(
 				builder.or(neverUpdated, updatedBeforeThreshold),
 				builder.or(disabledDateIsNull, disabledDateIsInPast));
 	}

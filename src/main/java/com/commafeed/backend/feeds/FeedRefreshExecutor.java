e3e476555a55196e439797b9b05b8432200d16fd
Athou <jeremiepanzer@gmail.com>
2013-07-28 12:44:24 +0200

    use default thread factory

--- src/main/java/com/commafeed/backend/feeds/FeedRefreshExecutor.java
+++ src/main/java/com/commafeed/backend/feeds/FeedRefreshExecutor.java
@@ -2,10 +2,8 @@ package com.commafeed.backend.feeds;
 
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -55,7 +53,6 @@ public class FeedRefreshExecutor {
 				}
 			}
 		});
-		pool.setThreadFactory(new NamedThreadFactory(poolName));
 	}
 
 	public void execute(Task task) {
@@ -82,27 +79,6 @@ public class FeedRefreshExecutor {
 			} catch (InterruptedException e) {
 				log.error("{} interrupted while waiting for threads to finish.", poolName);
 			}
-		}
-	}
-
-	private static class NamedThreadFactory implements ThreadFactory {
-		private final ThreadGroup group;
-		private final AtomicInteger threadNumber = new AtomicInteger(1);
-		private final String namePrefix;
-
-		private NamedThreadFactory(String poolName) {
-			SecurityManager s = System.getSecurityManager();
-			group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
-			namePrefix = poolName + "-thread-";
-		}
-
-		public Thread newThread(Runnable r) {
-			Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
-			if (t.isDaemon())
-				t.setDaemon(false);
-			if (t.getPriority() != Thread.NORM_PRIORITY)
-				t.setPriority(Thread.NORM_PRIORITY);
-			return t;
 		}
 	}
 }

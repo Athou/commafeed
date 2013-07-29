8d340e0f520b37d19808a9cd0683e93f5f576407
Athou <jeremiepanzer@gmail.com>
2013-07-29 21:31:08 +0200

    optimization not needed anymore

--- src/main/java/com/commafeed/frontend/rest/resources/UserREST.java
+++ src/main/java/com/commafeed/frontend/rest/resources/UserREST.java
@@ -70,12 +70,7 @@ public class UserREST extends AbstractResourceREST {
 		Settings s = new Settings();
 		UserSettings settings = userSettingsDAO.findByUser(getUser());
 		if (settings != null) {
-			if (applicationSettingsService.get().isHeavyLoad()) {
-				s.setReadingMode(ReadingMode.unread.name());
-			} else {
-				s.setReadingMode(settings.getReadingMode().name());
-			}
-
+			s.setReadingMode(settings.getReadingMode().name());
 			s.setReadingOrder(settings.getReadingOrder().name());
 			s.setViewMode(settings.getViewMode().name());
 			s.setShowRead(settings.isShowRead());

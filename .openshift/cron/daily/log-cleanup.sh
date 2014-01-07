5e1360a65b199174268ebfd7a8178a1ea65070ff
Athou <jeremiepanzer@gmail.com>
2014-01-07 12:02:15 +0100

    smarter log cleanup script (#533)

--- .openshift/cron/daily/log-cleanup.sh
+++ .openshift/cron/daily/log-cleanup.sh
@@ -1,1 +1,7 @@
-rm -rf $OPENSHIFT_JBOSSAS_LOG_DIR/*.log.*
+if [ $OPENSHIFT_JBOSSAS_LOG_DIR ]; then
+	rm -rf $OPENSHIFT_JBOSSAS_LOG_DIR/*.log.*
+fi
+
+if [ $OPENSHIFT_JBOSSEAP_LOG_DIR ]; then
+	rm -rf $OPENSHIFT_JBOSSEAP_LOG_DIR/*.log.*
+fi

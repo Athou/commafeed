6860940afccb355f4c4bde06df02a1694ab51580
Athou <jeremiepanzer@gmail.com>
2014-02-22 13:12:37 +0100

    fix javax.net.ssl.SSLProtocolException: handshake alert: unrecognized_name (fix #549)

--- conf/setenv.bat
+++ conf/setenv.bat
@@ -1,1 +1,1 @@
-set JAVA_OPTS=-Djava.net.preferIPv4Stack=true -Xmx1024m -XX:MaxPermSize=256m -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC
+set JAVA_OPTS=-Djava.net.preferIPv4Stack=true -Xmx1024m -XX:MaxPermSize=256m -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -Djsse.enableSNIExtension=false

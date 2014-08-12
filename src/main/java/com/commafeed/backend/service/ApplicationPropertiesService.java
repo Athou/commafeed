757cfff0e62268d289f340a57999e0b735458de2
Athou <jeremiepanzer@gmail.com>
2014-08-12 14:39:08 +0200

    cleanup

--- src/main/java/com/commafeed/backend/service/ApplicationPropertiesService.java
+++ src/main/java/com/commafeed/backend/service/ApplicationPropertiesService.java
@@ -10,19 +10,11 @@ public class ApplicationPropertiesService {
 		bundle = ResourceBundle.getBundle("application");
 	}
 
-	public String getDatasource() {
-		return bundle.getString("datasource");
-	}
-
 	public String getVersion() {
 		return bundle.getString("version");
 	}
 
 	public String getGitCommit() {
 		return bundle.getString("git.commit");
-	}
-
-	public boolean isProduction() {
-		return Boolean.valueOf(bundle.getString("production"));
 	}
 }

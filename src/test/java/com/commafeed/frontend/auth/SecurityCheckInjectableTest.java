f9e9a4547c9fb0426a6ca985cc7b79699c4e6f05
Athou <jeremiepanzer@gmail.com>
2014-10-22 10:34:55 +0200

    remove unused variable

--- src/test/java/com/commafeed/frontend/auth/SecurityCheckInjectableTest.java
+++ src/test/java/com/commafeed/frontend/auth/SecurityCheckInjectableTest.java
@@ -17,86 +17,86 @@ import com.commafeed.frontend.auth.SecurityCheckProvider.SecurityCheckInjectable
 import com.google.common.base.Optional;
 
 public class SecurityCheckInjectableTest {
-	
+
 	private static String SESSION_KEY_USER = "user";
-	
-	@Test public void
-	cookie_login_does_not_create_a_session_if_not_present() {
+
+	@Test
+	public void cookie_login_does_not_create_a_session_if_not_present() {
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		UserService service = mock(UserService.class);
-		
+
 		SecurityCheckInjectable injectable = new SecurityCheckInjectable(request, service, null, false);
 		injectable.cookieSessionLogin();
-		
+
 		verify(request).getSession(false);
 	}
-	
-	@Test public void
-	cookie_login_should_not_return_user_if_there_is_no_preexisting_http_session() {
+
+	@Test
+	public void cookie_login_should_not_return_user_if_there_is_no_preexisting_http_session() {
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		when(request.getSession(false)).thenReturn(null);
-		
+
 		UserService service = new UserService(null, null, null, null, null, null);
-		
+
 		SecurityCheckInjectable injectable = new SecurityCheckInjectable(request, service, null, false);
 		Optional<User> user = injectable.cookieSessionLogin();
-		
+
 		Assert.assertFalse(user.isPresent());
 	}
-	
-	@Test public void
-	cookie_login_should_not_return_user_if_user_not_present_in_http_session() {
+
+	@Test
+	public void cookie_login_should_not_return_user_if_user_not_present_in_http_session() {
 		HttpSession session = mock(HttpSession.class);
 		when(session.getAttribute(SESSION_KEY_USER)).thenReturn(null);
-		
+
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		when(request.getSession(false)).thenReturn(session);
-		
+
 		UserService service = new UserService(null, null, null, null, null, null);
-		
+
 		SecurityCheckInjectable injectable = new SecurityCheckInjectable(request, service, null, false);
 		Optional<User> user = injectable.cookieSessionLogin();
-		
+
 		Assert.assertFalse(user.isPresent());
 	}
-	
-	@Test public void
-	cookie_login_should_perform_post_login_activities_if_user_present_in_http_session() {
+
+	@Test
+	public void cookie_login_should_perform_post_login_activities_if_user_present_in_http_session() {
 		User userInSession = new User();
-		
+
 		HttpSession session = mock(HttpSession.class);
 		when(session.getAttribute(SESSION_KEY_USER)).thenReturn(userInSession);
-		
+
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		when(request.getSession(false)).thenReturn(session);
-		
+
 		PostLoginActivities postLoginActivities = mock(PostLoginActivities.class);
-		
+
 		UserService service = new UserService(null, null, null, null, null, postLoginActivities);
-		
+
 		SecurityCheckInjectable injectable = new SecurityCheckInjectable(request, service, null, false);
-		Optional<User> user = injectable.cookieSessionLogin();
-		
+		injectable.cookieSessionLogin();
+
 		verify(postLoginActivities).executeFor(userInSession);
 	}
-	
-	@Test public void
-	cookie_login_should_return_user_if_user_present_in_http_session() {
+
+	@Test
+	public void cookie_login_should_return_user_if_user_present_in_http_session() {
 		User userInSession = new User();
-		
+
 		HttpSession session = mock(HttpSession.class);
 		when(session.getAttribute(SESSION_KEY_USER)).thenReturn(userInSession);
-		
+
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		when(request.getSession(false)).thenReturn(session);
-		
+
 		PostLoginActivities postLoginActivities = mock(PostLoginActivities.class);
-		
+
 		UserService service = new UserService(null, null, null, null, null, postLoginActivities);
-		
+
 		SecurityCheckInjectable injectable = new SecurityCheckInjectable(request, service, null, false);
 		Optional<User> user = injectable.cookieSessionLogin();
-		
+
 		Assert.assertTrue(user.isPresent());
 		Assert.assertEquals(userInSession, user.get());
 	}

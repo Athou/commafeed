f409af1c377b8c4282b20010be60d473970741a5
Athou <jeremiepanzer@gmail.com>
2014-10-26 12:25:44 +0100

    rewrite favicon fetcher

--- src/main/java/com/commafeed/CommaFeedModule.java
+++ src/main/java/com/commafeed/CommaFeedModule.java
@@ -11,8 +11,12 @@ import com.commafeed.CommaFeedConfiguration.CacheType;
 import com.commafeed.backend.cache.CacheService;
 import com.commafeed.backend.cache.NoopCacheService;
 import com.commafeed.backend.cache.RedisCacheService;
+import com.commafeed.backend.favicon.DefaultFaviconFetcher;
+import com.commafeed.backend.favicon.AbstractFaviconFetcher;
+import com.commafeed.backend.favicon.YoutubeFaviconFetcher;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.multibindings.Multibinder;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -33,5 +37,9 @@ public class CommaFeedModule extends AbstractModule {
 				: new RedisCacheService(config.getRedisPoolFactory().build());
 		log.info("using cache {}", cacheService.getClass());
 		bind(CacheService.class).toInstance(cacheService);
+
+		Multibinder<AbstractFaviconFetcher> multibinder = Multibinder.newSetBinder(binder(), AbstractFaviconFetcher.class);
+		multibinder.addBinding().to(YoutubeFaviconFetcher.class);
+		multibinder.addBinding().to(DefaultFaviconFetcher.class);
 	}
 }

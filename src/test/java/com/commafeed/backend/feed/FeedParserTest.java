9c462b5f572952c08b6539549a3593aa25b4df83
Winterhart <m.user.work@gmail.com>
2018-02-24 10:12:54 -0500

    Addition of the first test class to test FeedParser.java

---
+++ src/test/java/com/commafeed/backend/feed/FeedParserTest.java
@@ -0,0 +1,12 @@
+package com.commafeed.backend.feed;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+
+/**
+ * This class will test methods of the class FeedParser
+ */
+public class FeedParserTest {
+
+
+}

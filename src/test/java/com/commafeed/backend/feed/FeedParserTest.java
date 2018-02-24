809060ab72a210c7803d11909f85a0d4dda5d816
Winterhart <m.user.work@gmail.com>
2018-02-24 16:36:22 -0500

    Addition of simple test for method getContent() and getTitle, addition of method to fake data SyndEntry

--- src/test/java/com/commafeed/backend/feed/FeedParserTest.java
+++ src/test/java/com/commafeed/backend/feed/FeedParserTest.java
@@ -3,6 +3,7 @@ import com.commafeed.backend.model.Feed;
 import com.rometools.rome.feed.CopyFrom;
 import com.rometools.rome.feed.module.Module;
 import com.rometools.rome.feed.synd.*;
+import org.apache.commons.lang3.StringUtils;
 import org.jdom2.Element;
 import org.junit.Assert;
 import org.junit.Test;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This class will test methods of the class FeedParser.java
@@ -68,7 +70,7 @@ public class FeedParserTest {
     public void testgetEntryUpdateDate(){
         childOfParser parser = mock(childOfParser.class);
         // Make a fake SyncEntry
-        SyndEntry fakeEntry = makeFakeSyndEntry();
+        SyndEntry fakeEntry = makeMockFakeSyndEntry();
         Date now = new Date();
         fakeEntry.setUpdatedDate(now);
 
@@ -90,28 +92,72 @@ public class FeedParserTest {
         fakeEntry.setUpdatedDate(null);
         when(parser.getEntryUpdateDate(fakeEntry)).thenReturn(testDateEarly);
         Assert.assertEquals(parser.getEntryUpdateDate(fakeEntry), testDateEarly);
-        
+    }
+    @Test
+    public void testGetContent(){
+        // In this section , it was not possible to use Mockito as the library org.apache.common.lang3
+        // was not initialized correctly using mock(class)...
+        childOfParser parser = new childOfParser();
+        // Make a fake SyncEntry
+        SyndEntry fakeEntryWithNothing = mock(SyndEntry.class);
+        // Test with no value
+        Assert.assertEquals(parser.getContent(fakeEntryWithNothing), null);
 
+        SyndEntry fakeEntry = makeFakeSyndEntry();
+        String expectedValue = fakeEntry.getContents().get(0).getValue();
 
-
+        Assert.assertEquals(parser.getContent(fakeEntry), expectedValue);
+    }
+    @Test
+    public void testGetTitle(){
+        // In this section , it was not possible to use Mockito as the library org.apache.common.lang3
+        // was not initialized correctly using mock(class)...
+        childOfParser parser = new childOfParser();
+        // Make a fake SyncEntry
+        SyndEntry fakeEntry = makeFakeSyndEntry();
+        String expectedTitle = fakeEntry.getTitle();
+        Assert.assertEquals(parser.getTitle(fakeEntry), expectedTitle);
 
     }
-    // Helper function to set a fake entry...
-    private SyndEntry makeFakeSyndEntry(){
-        SyndEntry fakeSyncEntry = mock(SyndEntry.class);
+
+    // make a mock fake SyndEntry
+    private SyndEntry makeMockFakeSyndEntry(){
+        SyndEntry fakeMockSyncEntry = mock(SyndEntry.class);
         SyndContent fakeContent = mock(SyndContent.class);
 
         fakeContent.setValue("This is fake Content");
+        fakeContent.setType("News");
+        fakeContent.setMode("standard");
 
         List<SyndContent> fakeList = mock(ArrayList.class);
-
         fakeList.add(fakeContent);
 
+        fakeMockSyncEntry.setAuthor("Bob Bobinson");
+        fakeMockSyncEntry.setLink("http://this.is.a.link.com");
+        fakeMockSyncEntry.setContents(fakeList);
+        fakeMockSyncEntry.setDescription(fakeContent);
+        fakeMockSyncEntry.setUri("http://this-is-a-website.com");
+        return  fakeMockSyncEntry;
+    }
+    // Helper function to set a fake entry...
+    private SyndEntry makeFakeSyndEntry(){
+        SyndEntry fakeSyncEntry = new SyndEntryImpl();
+        SyndContent fakeContent = new SyndContentImpl();
+
+        fakeContent.setValue("This is fake Content");
+        fakeContent.setType("News");
+        fakeContent.setMode("standard");
+
+        List<SyndContent> fakeList = new ArrayList<SyndContent>();
+        fakeList.add(0, fakeContent);
+
+        fakeSyncEntry.setComments("This is a comment");
         fakeSyncEntry.setAuthor("Bob Bobinson");
         fakeSyncEntry.setLink("http://this.is.a.link.com");
         fakeSyncEntry.setContents(fakeList);
         fakeSyncEntry.setDescription(fakeContent);
         fakeSyncEntry.setUri("http://this-is-a-website.com");
+        fakeSyncEntry.setTitle("This is a title");
         return  fakeSyncEntry;
     }
 }

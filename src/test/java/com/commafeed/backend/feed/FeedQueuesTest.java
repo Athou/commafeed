f979bed8b688e92ccc86aa09f35388cef7675e9e
Philippe Beaudry <philippe.beaudry96@gmail.com>
2018-02-25 18:21:41 -0500

    added test for the add function in feedQueues

--- src/test/java/com/commafeed/backend/feed/FeedQueuesTest.java
+++ src/test/java/com/commafeed/backend/feed/FeedQueuesTest.java
@@ -1,14 +1,33 @@
 package com.commafeed.backend.feed;
 
+import com.commafeed.backend.model.Feed;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mock;
 
 
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import static org.mockito.Mockito.*;
 
 
 public class FeedQueuesTest {
+
+    @Test
+    public void testAdd(){
+        //create a fake feed queues objects by mocking
+        FeedQueues fakeFeedQueues = mock(FeedQueues.class);
+
+        Feed fakeFeed = mock(Feed.class);
+
+        //Simple mocking and verifying
+        doNothing().when(fakeFeedQueues).add(isA(Feed.class), isA(boolean.class));
+
+        fakeFeedQueues.add(fakeFeed, true);
+
+        verify(fakeFeedQueues, times(1)).add(fakeFeed, true);
+    }
 
 
     @Test

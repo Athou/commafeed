3e442df943d417252acdda80218d08a234a4d8c7
Benjamin T <Benjamin.tanguay@gmail.com>
2018-04-02 19:11:00 -0400

    Creation of an example of a storage using the UserSettings model as a basis
    Creation of an interface to be used with the storage so that commonly required methods are implemented
    
    >
    >
    Co-authored-by: philbeaudry <philippe.beaudry96@gmail.com>

--- src/main/java/com/commafeed/backend/dao/newstorage/UserSettingsStorage.java
+++ src/main/java/com/commafeed/backend/dao/newstorage/UserSettingsStorage.java
@@ -2,13 +2,16 @@ package com.commafeed.backend.dao.newstorage;
 
 import com.commafeed.backend.model.UserSettings;
 
-public class UserSettingsStorage extends GenericStorage<String, UserSettings> {
+public class UserSettingsStorage implements
+        IStorageModelDAO<UserSettings> {
 
+    private UserSettings model;
+    private GenericStorage<Long, UserSettings> storage;
     private static UserSettingsStorage instance;
 
     private UserSettingsStorage() {
         // Provide the name of the file it will be serialized to
-        super("UserSettings");
+        this.storage = new GenericStorage<Long, UserSettings>("UserSettings");
     }
 
     public static UserSettingsStorage getInstance() {
@@ -20,5 +23,63 @@ public class UserSettingsStorage extends GenericStorage<String, UserSettings> {
 
     public static UserSettingsStorage getTestInstance() {
         return new UserSettingsStorage();
+    }
+
+    @Override
+    public boolean exists(UserSettings model) {
+        return this.storage.exists(model.getUser().getId());
+    }
+
+    @Override
+    public void create(UserSettings model) {
+        this.storage.create(model.getUser().getId(), model);
+    }
+
+    @Override
+    public UserSettings read(Long id) {
+        return this.storage.read(id);
+    }
+
+    @Override
+    public UserSettings read(UserSettings model) {
+        return read(model.getUser().getId());
+    }
+
+    @Override
+    public UserSettings update(UserSettings model) {
+        return this.storage.update(model.getUser().getId(), model);
+    }
+
+    @Override
+    public UserSettings delete(UserSettings model) {
+        return this.storage.delete(model.getUser().getId());
+    }
+
+    @Override
+    public void serialize() {
+        this.storage.saveStorage();
+    }
+
+    @Override
+    public void deserialize() {
+        this.storage.loadStorage();
+    }
+
+    @Override
+    public boolean isModelConsistent(UserSettings model) {
+        UserSettings modelFromStorage = read(model);
+        if (model.equals(modelFromStorage)) {
+            return true;
+        } else {
+            update(model);
+            verification(model, modelFromStorage);
+            return false;
+        }
+    }
+
+    public void verification(UserSettings expected, UserSettings received) {
+        System.out.println("Inconsistency found!\n\nObject in real database: " +
+                "" + expected +
+                "\n\nObject found in new storage: " + received);
     }
 }

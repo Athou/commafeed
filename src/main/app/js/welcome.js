0a97f042572c30b720595ed51fc8724846937263
Athou <jeremiepanzer@gmail.com>
2022-01-01 21:26:02 +0100

    apply prettier

--- src/main/app/js/welcome.js
+++ src/main/app/js/welcome.js
@@ -1,7 +1,7 @@
-$(function() {
-	var reg = $('#register-panel');
-	if (!reg) {
-		return;
-	}
-	$('#login-panel').height(reg.height());
-});
+$(function () {
+    var reg = $("#register-panel")
+    if (!reg) {
+        return
+    }
+    $("#login-panel").height(reg.height())
+})

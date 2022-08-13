6c6cc8d85bcc9a04dce7dcaf58bc8a98d9a6cb75
Athou <jeremiepanzer@gmail.com>
2022-08-13 18:05:24 +0200

    return smaller error when trying to subscribe to an invalid feed

--- commafeed-server/src/main/java/com/commafeed/frontend/resource/FeedREST.java
+++ commafeed-server/src/main/java/com/commafeed/frontend/resource/FeedREST.java
@@ -272,8 +272,9 @@ public class FeedREST {
 		try {
 			info = fetchFeedInternal(req.getUrl());
 		} catch (Exception e) {
+			Throwable cause = Throwables.getRootCause(e);
 			return Response.status(Status.INTERNAL_SERVER_ERROR)
-					.entity(Throwables.getStackTraceAsString(Throwables.getRootCause(e)))
+					.entity(cause.getClass().getName() + ": " + cause.getMessage())
 					.type(MediaType.TEXT_PLAIN)
 					.build();
 		}

8bc6a2adcc63d419a6657b893882f491d7edd15d
Athou <jeremiepanzer@gmail.com>
2022-08-22 13:43:14 +0200

    remove StrictMode, it doesn't really help and causes all components to be rendered twice in dev

--- commafeed-client/src/main.tsx
+++ commafeed-client/src/main.tsx
@@ -2,7 +2,6 @@ import "@fontsource/open-sans"
 import { store } from "app/store"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
-import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import "swagger-ui-react/swagger-ui.css"
@@ -11,9 +10,7 @@ import { App } from "./App"
 dayjs.extend(relativeTime)
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
 )

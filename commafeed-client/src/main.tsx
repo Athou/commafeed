b5e59896045b42721a7cfab9c087728193d2872e
Athou <jeremiepanzer@gmail.com>
2023-05-27 19:54:02 +0200

    disable pull-to-refresh on mobile as it messes with vertical scrolling

--- commafeed-client/src/main.tsx
+++ commafeed-client/src/main.tsx
@@ -1,11 +1,12 @@
 import "@fontsource/open-sans"
+import { App } from "App"
 import { store } from "app/store"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
+import "main.css"
 import "react-contexify/ReactContexify.css"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
-import { App } from "./App"
 
 dayjs.extend(relativeTime)
 

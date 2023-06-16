6944d4dc0b7864368a1ea218f574296ea13ba47e
Athou <jeremiepanzer@gmail.com>
2023-06-16 20:07:36 +0200

    fix unreadable api documentation page with dark theme (#1082)

--- commafeed-client/src/pages/app/ApiDocumentationPage.tsx
+++ commafeed-client/src/pages/app/ApiDocumentationPage.tsx
@@ -1,8 +1,14 @@
+import { Box } from "@mantine/core"
 import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
 
 function ApiDocumentationPage() {
-    return <SwaggerUI url="swagger/swagger.json" />
+    return (
+        // force white background because swagger is unreadable with dark theme
+        <Box style={{ backgroundColor: "#fff" }}>
+            <SwaggerUI url="swagger/swagger.json" />
+        </Box>
+    )
 }
 
 export default ApiDocumentationPage

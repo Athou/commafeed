c2e453027c13a42c4e2c07c75d1e3352d9b69014
Athou <jeremiepanzer@gmail.com>
2023-08-16 07:36:05 +0200

    fix desktop entry header shortly rendered as mobile, causing a small visual glitch

--- commafeed-client/src/hooks/useMobile.ts
+++ commafeed-client/src/hooks/useMobile.ts
@@ -1,4 +1,7 @@
 import { useMediaQuery } from "@mantine/hooks"
 import { Constants } from "app/constants"
 
-export const useMobile = (breakpoint: string = Constants.layout.mobileBreakpoint) => !useMediaQuery(`(min-width: ${breakpoint})`)
+export const useMobile = (breakpoint: string = Constants.layout.mobileBreakpoint) =>
+    !useMediaQuery(`(min-width: ${breakpoint})`, undefined, {
+        getInitialValueInEffect: false,
+    })

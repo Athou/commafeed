b2bd386e9c0ffb2bb8d236bb2c7ba353e01223a8
Athou <jeremiepanzer@gmail.com>
2023-12-24 10:52:09 +0100

    reset database completely after each test so that tests cannot impact each other

--- commafeed-server/src/test/java/com/commafeed/CommaFeedDropwizardAppExtension.java
+++ commafeed-server/src/test/java/com/commafeed/CommaFeedDropwizardAppExtension.java
@@ -1,10 +1,7 @@
 package com.commafeed;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -14,9 +11,6 @@ import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 
 public class CommaFeedDropwizardAppExtension extends DropwizardAppExtension<CommaFeedConfiguration> {
-
-	private static final List<String> TABLES = Arrays.asList("FEEDENTRYSTATUSES", "FEEDENTRYTAGS", "FEEDENTRIES", "FEEDENTRYCONTENTS",
-			"FEEDSUBSCRIPTIONS", "FEEDS", "FEEDCATEGORIES");
 
 	public CommaFeedDropwizardAppExtension() {
 		super(CommaFeedApplication.class, ResourceHelpers.resourceFilePath("config.test.yml"));
@@ -29,10 +23,7 @@ public class CommaFeedDropwizardAppExtension extends DropwizardAppExtension<Comm
 		// clean database after each test
 		DataSource dataSource = getConfiguration().getDataSourceFactory().build(new MetricRegistry(), "cleanup");
 		try (Connection connection = dataSource.getConnection()) {
-			for (String table : TABLES) {
-				PreparedStatement statement = connection.prepareStatement("DELETE FROM " + table);
-				statement.executeUpdate();
-			}
+			connection.prepareStatement("DROP ALL OBJECTS").executeUpdate();
 		} catch (SQLException e) {
 			throw new RuntimeException("could not cleanup database", e);
 		}

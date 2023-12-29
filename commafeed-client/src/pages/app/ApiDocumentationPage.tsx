39ba4a1c97a2abf2a09a5e79095979f6c67571d6
Athou <jeremiepanzer@gmail.com>
2023-12-29 12:11:33 +0100

    disable redoc url sync because it causes issues with hashrouter

--- commafeed-client/src/pages/app/ApiDocumentationPage.tsx
+++ commafeed-client/src/pages/app/ApiDocumentationPage.tsx
@@ -1,5 +1,10 @@
 import { Box } from "@mantine/core"
-import { RedocStandalone } from "redoc"
+import { HistoryService, RedocStandalone } from "redoc"
+
+// disable redoc url sync because it causes issues with hashrouter
+Object.defineProperty(HistoryService.prototype, "replace", {
+    value: () => {},
+})
 
 function ApiDocumentationPage() {
     return (

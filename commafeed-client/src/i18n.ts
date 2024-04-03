d610f980c738fb23987c82791c05a578a02a4612
Athou <jeremiepanzer@gmail.com>
2024-04-03 09:11:06 +0200

    fix typo

--- commafeed-client/src/i18n.ts
+++ commafeed-client/src/i18n.ts
@@ -6,40 +6,40 @@ import { useEffect } from "react"
 interface Locale {
     key: string
     label: string
-    daysjsImportFn: () => Promise<ILocale>
+    dayjsImportFn: () => Promise<ILocale>
 }
 
 // add an object to the array to add a new locale
 // don't forget to also add it to the 'locales' array in .linguirc
 export const locales: Locale[] = [
-    { key: "ar", label: "العربية", daysjsImportFn: async () => await import("dayjs/locale/ar") },
-    { key: "ca", label: "Català", daysjsImportFn: async () => await import("dayjs/locale/ca") },
-    { key: "cs", label: "Čeština", daysjsImportFn: async () => await import("dayjs/locale/cs") },
-    { key: "cy", label: "Cymraeg", daysjsImportFn: async () => await import("dayjs/locale/cy") },
-    { key: "da", label: "Danish", daysjsImportFn: async () => await import("dayjs/locale/da") },
-    { key: "de", label: "Deutsch", daysjsImportFn: async () => await import("dayjs/locale/de") },
-    { key: "en", label: "English", daysjsImportFn: async () => await import("dayjs/locale/en") },
-    { key: "es", label: "Español", daysjsImportFn: async () => await import("dayjs/locale/es") },
-    { key: "fa", label: "فارسی", daysjsImportFn: async () => await import("dayjs/locale/fa") },
-    { key: "fi", label: "Suomi", daysjsImportFn: async () => await import("dayjs/locale/fi") },
-    { key: "fr", label: "Français", daysjsImportFn: async () => await import("dayjs/locale/fr") },
-    { key: "gl", label: "Galician", daysjsImportFn: async () => await import("dayjs/locale/gl") },
-    { key: "hu", label: "Magyar", daysjsImportFn: async () => await import("dayjs/locale/hu") },
-    { key: "id", label: "Indonesian", daysjsImportFn: async () => await import("dayjs/locale/id") },
-    { key: "it", label: "Italiano", daysjsImportFn: async () => await import("dayjs/locale/it") },
-    { key: "ja", label: "日本語", daysjsImportFn: async () => await import("dayjs/locale/ja") },
-    { key: "ko", label: "한국어", daysjsImportFn: async () => await import("dayjs/locale/ko") },
-    { key: "ms", label: "Bahasa Malaysian", daysjsImportFn: async () => await import("dayjs/locale/ms") },
-    { key: "nb", label: "Norsk (bokmål)", daysjsImportFn: async () => await import("dayjs/locale/nb") },
-    { key: "nl", label: "Nederlands", daysjsImportFn: async () => await import("dayjs/locale/nl") },
-    { key: "nn", label: "Norsk (nynorsk)", daysjsImportFn: async () => await import("dayjs/locale/nn") },
-    { key: "pl", label: "Polski", daysjsImportFn: async () => await import("dayjs/locale/pl") },
-    { key: "pt", label: "Português", daysjsImportFn: async () => await import("dayjs/locale/pt") },
-    { key: "ru", label: "Русский", daysjsImportFn: async () => await import("dayjs/locale/ru") },
-    { key: "sk", label: "Slovenčina", daysjsImportFn: async () => await import("dayjs/locale/sk") },
-    { key: "sv", label: "Svenska", daysjsImportFn: async () => await import("dayjs/locale/sv") },
-    { key: "tr", label: "Türkçe", daysjsImportFn: async () => await import("dayjs/locale/tr") },
-    { key: "zh", label: "简体中文", daysjsImportFn: async () => await import("dayjs/locale/zh") },
+    { key: "ar", label: "العربية", dayjsImportFn: async () => await import("dayjs/locale/ar") },
+    { key: "ca", label: "Català", dayjsImportFn: async () => await import("dayjs/locale/ca") },
+    { key: "cs", label: "Čeština", dayjsImportFn: async () => await import("dayjs/locale/cs") },
+    { key: "cy", label: "Cymraeg", dayjsImportFn: async () => await import("dayjs/locale/cy") },
+    { key: "da", label: "Danish", dayjsImportFn: async () => await import("dayjs/locale/da") },
+    { key: "de", label: "Deutsch", dayjsImportFn: async () => await import("dayjs/locale/de") },
+    { key: "en", label: "English", dayjsImportFn: async () => await import("dayjs/locale/en") },
+    { key: "es", label: "Español", dayjsImportFn: async () => await import("dayjs/locale/es") },
+    { key: "fa", label: "فارسی", dayjsImportFn: async () => await import("dayjs/locale/fa") },
+    { key: "fi", label: "Suomi", dayjsImportFn: async () => await import("dayjs/locale/fi") },
+    { key: "fr", label: "Français", dayjsImportFn: async () => await import("dayjs/locale/fr") },
+    { key: "gl", label: "Galician", dayjsImportFn: async () => await import("dayjs/locale/gl") },
+    { key: "hu", label: "Magyar", dayjsImportFn: async () => await import("dayjs/locale/hu") },
+    { key: "id", label: "Indonesian", dayjsImportFn: async () => await import("dayjs/locale/id") },
+    { key: "it", label: "Italiano", dayjsImportFn: async () => await import("dayjs/locale/it") },
+    { key: "ja", label: "日本語", dayjsImportFn: async () => await import("dayjs/locale/ja") },
+    { key: "ko", label: "한국어", dayjsImportFn: async () => await import("dayjs/locale/ko") },
+    { key: "ms", label: "Bahasa Malaysian", dayjsImportFn: async () => await import("dayjs/locale/ms") },
+    { key: "nb", label: "Norsk (bokmål)", dayjsImportFn: async () => await import("dayjs/locale/nb") },
+    { key: "nl", label: "Nederlands", dayjsImportFn: async () => await import("dayjs/locale/nl") },
+    { key: "nn", label: "Norsk (nynorsk)", dayjsImportFn: async () => await import("dayjs/locale/nn") },
+    { key: "pl", label: "Polski", dayjsImportFn: async () => await import("dayjs/locale/pl") },
+    { key: "pt", label: "Português", dayjsImportFn: async () => await import("dayjs/locale/pt") },
+    { key: "ru", label: "Русский", dayjsImportFn: async () => await import("dayjs/locale/ru") },
+    { key: "sk", label: "Slovenčina", dayjsImportFn: async () => await import("dayjs/locale/sk") },
+    { key: "sv", label: "Svenska", dayjsImportFn: async () => await import("dayjs/locale/sv") },
+    { key: "tr", label: "Türkçe", dayjsImportFn: async () => await import("dayjs/locale/tr") },
+    { key: "zh", label: "简体中文", dayjsImportFn: async () => await import("dayjs/locale/zh") },
 ]
 
 function activateLocale(locale: string) {
@@ -52,7 +52,7 @@ function activateLocale(locale: string) {
     // dayjs
     locales
         .find(l => l.key === locale)
-        ?.daysjsImportFn()
+        ?.dayjsImportFn()
         .then(() => dayjs.locale(locale))
 }
 

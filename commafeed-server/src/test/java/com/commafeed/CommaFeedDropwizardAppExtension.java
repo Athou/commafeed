dcc32cb5393379506c7b0e6ae1efa4dd0272f209
Athou <jeremiepanzer@gmail.com>
2024-04-14 16:49:26 +0200

    use a random available port for tests

--- commafeed-server/src/test/java/com/commafeed/CommaFeedDropwizardAppExtension.java
+++ commafeed-server/src/test/java/com/commafeed/CommaFeedDropwizardAppExtension.java
@@ -5,15 +5,19 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.mockserver.socket.PortFactory;
+
 import com.codahale.metrics.MetricRegistry;
 
+import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
 
 public class CommaFeedDropwizardAppExtension extends DropwizardAppExtension<CommaFeedConfiguration> {
 
 	public CommaFeedDropwizardAppExtension() {
-		super(CommaFeedApplication.class, ResourceHelpers.resourceFilePath("config.test.yml"));
+		super(CommaFeedApplication.class, ResourceHelpers.resourceFilePath("config.test.yml"),
+				ConfigOverride.config("server.applicationConnectors[0].port", String.valueOf(PortFactory.findFreePort())));
 	}
 
 	@Override

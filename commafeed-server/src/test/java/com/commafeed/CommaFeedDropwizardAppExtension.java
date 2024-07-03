dba944874b218f57a710b0eadec2105b07a191a5
Athou <jeremiepanzer@gmail.com>
2024-07-03 10:41:01 +0200

    add redis integration test

--- commafeed-server/src/test/java/com/commafeed/CommaFeedDropwizardAppExtension.java
+++ commafeed-server/src/test/java/com/commafeed/CommaFeedDropwizardAppExtension.java
@@ -12,19 +12,25 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.mockserver.socket.PortFactory;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.MariaDBContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import com.codahale.metrics.MetricRegistry;
+import com.commafeed.CommaFeedConfiguration.CacheType;
 
 import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 public class CommaFeedDropwizardAppExtension extends DropwizardAppExtension<CommaFeedConfiguration> {
 	private static final String TEST_DATABASE = System.getenv().getOrDefault("TEST_DATABASE", "h2");
+	private static final boolean REDIS_ENABLED = Boolean.parseBoolean(System.getenv().getOrDefault("REDIS", "false"));
 
 	private static final ConfigOverride[] CONFIG_OVERRIDES;
 	private static final List<String> DROP_ALL_STATEMENTS;
@@ -32,7 +38,9 @@ public class CommaFeedDropwizardAppExtension extends DropwizardAppExtension<Comm
 		List<ConfigOverride> overrides = new ArrayList<>();
 		overrides.add(ConfigOverride.config("server.applicationConnectors[0].port", String.valueOf(PortFactory.findFreePort())));
 
-		DatabaseConfiguration config = buildConfiguration(TEST_DATABASE);
+		Properties imageNames = readProperties("/docker-images.properties");
+
+		DatabaseConfiguration config = buildConfiguration(TEST_DATABASE, imageNames.getProperty(TEST_DATABASE));
 		JdbcDatabaseContainer<?> container = config.container();
 		if (container != null) {
 			container.withDatabaseName("commafeed");
@@ -45,6 +53,16 @@ public class CommaFeedDropwizardAppExtension extends DropwizardAppExtension<Comm
 			overrides.add(ConfigOverride.config("database.driverClass", container.getDriverClassName()));
 		}
 
+		if (REDIS_ENABLED) {
+			GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse(imageNames.getProperty("redis")))
+					.withExposedPorts(6379);
+			redis.start();
+
+			overrides.add(ConfigOverride.config("app.cache", "redis"));
+			overrides.add(ConfigOverride.config("redis.host", redis.getHost()));
+			overrides.add(ConfigOverride.config("redis.port", redis.getMappedPort(6379).toString()));
+		}
+
 		CONFIG_OVERRIDES = overrides.toArray(new ConfigOverride[0]);
 		DROP_ALL_STATEMENTS = config.dropAllStatements();
 	}
@@ -53,15 +71,7 @@ public class CommaFeedDropwizardAppExtension extends DropwizardAppExtension<Comm
 		super(CommaFeedApplication.class, ResourceHelpers.resourceFilePath("config.test.yml"), CONFIG_OVERRIDES);
 	}
 
-	private static DatabaseConfiguration buildConfiguration(String databaseName) {
-		Properties properties = new Properties();
-		try (InputStream is = CommaFeedDropwizardAppExtension.class.getResourceAsStream("/docker-images.properties")) {
-			properties.load(is);
-		} catch (IOException e) {
-			throw new RuntimeException("could not read docker-images.properties", e);
-		}
-
-		String imageName = properties.getProperty(databaseName);
+	private static DatabaseConfiguration buildConfiguration(String databaseName, String imageName) {
 		if ("postgresql".equals(databaseName)) {
 			JdbcDatabaseContainer<?> container = new PostgreSQLContainer<>(imageName).withTmpFs(Map.of("/var/lib/postgresql/data", "rw"));
 			return new DatabaseConfiguration(container, List.of("DROP SCHEMA public CASCADE", "CREATE SCHEMA public"));
@@ -77,6 +87,16 @@ public class CommaFeedDropwizardAppExtension extends DropwizardAppExtension<Comm
 		}
 	}
 
+	private static Properties readProperties(String path) {
+		Properties properties = new Properties();
+		try (InputStream is = CommaFeedDropwizardAppExtension.class.getResourceAsStream(path)) {
+			properties.load(is);
+		} catch (IOException e) {
+			throw new RuntimeException("could not read resource " + path, e);
+		}
+		return properties;
+	}
+
 	@Override
 	public void after() {
 		super.after();
@@ -89,6 +109,13 @@ public class CommaFeedDropwizardAppExtension extends DropwizardAppExtension<Comm
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException("could not cleanup database", e);
+		}
+
+		// clean redis cache after each test
+		if (getConfiguration().getApplicationSettings().getCache() == CacheType.REDIS) {
+			try (JedisPool pool = getConfiguration().getRedisPoolFactory().build(); Jedis jedis = pool.getResource()) {
+				jedis.flushAll();
+			}
 		}
 	}
 

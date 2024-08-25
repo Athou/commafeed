63c76790674c5d3f0945ef37ebbc5c0c65fd9ace
Athou <jeremiepanzer@gmail.com>
2024-08-26 00:17:42 +0200

    make sure the webapp and openapi documentation are always up to date by preventing caching

---
+++ commafeed-server/src/test/java/com/commafeed/integration/StaticFilesIT.java
@@ -0,0 +1,23 @@
+package com.commafeed.integration;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+
+@QuarkusTest
+class StaticFilesIT {
+
+	@ParameterizedTest
+	@ValueSource(strings = { "/", "/openapi.json", "/openapi.yaml" })
+	void servedWithoutCache(String path) {
+		RestAssured.given().when().get(path).then().statusCode(200).header("Cache-Control", "no-cache, no-store, must-revalidate");
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { "/favicon.ico" })
+	void servedWithCache(String path) {
+		RestAssured.given().when().get(path).then().statusCode(200).header("Cache-Control", "public, immutable, max-age=86400");
+	}
+}

a565566c50bea668bbb2f4b0f6ba284455601ccd
Athou <jeremiepanzer@gmail.com>
2024-08-27 09:02:42 +0200

    remove deprecation warning

--- commafeed-server/src/test/java/com/commafeed/DatabaseReset.java
+++ commafeed-server/src/test/java/com/commafeed/DatabaseReset.java
@@ -4,12 +4,10 @@ import org.kohsuke.MetaInfServices;
 
 import com.commafeed.backend.service.db.DatabaseStartupService;
 
-import io.quarkus.liquibase.LiquibaseFactory;
+import io.quarkus.liquibase.runtime.LiquibaseSchemaProvider;
 import io.quarkus.test.junit.callback.QuarkusTestBeforeEachCallback;
 import io.quarkus.test.junit.callback.QuarkusTestMethodContext;
 import jakarta.enterprise.inject.spi.CDI;
-import liquibase.Liquibase;
-import liquibase.exception.LiquibaseException;
 
 /**
  * Resets database between tests
@@ -17,18 +15,9 @@ import liquibase.exception.LiquibaseException;
 @MetaInfServices
 public class DatabaseReset implements QuarkusTestBeforeEachCallback {
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void beforeEach(QuarkusTestMethodContext context) {
-		LiquibaseFactory liquibaseFactory = CDI.current().select(LiquibaseFactory.class).get();
-		try (Liquibase liquibase = liquibaseFactory.createLiquibase()) {
-			liquibase.dropAll();
-			liquibase.update(liquibaseFactory.createContexts(), liquibaseFactory.createLabels());
-		} catch (LiquibaseException e) {
-			throw new RuntimeException(e);
-		}
-
-		DatabaseStartupService databaseStartupService = CDI.current().select(DatabaseStartupService.class).get();
-		databaseStartupService.populateInitialData();
+		new LiquibaseSchemaProvider().resetAllDatabases();
+		CDI.current().select(DatabaseStartupService.class).get().populateInitialData();
 	}
 }

cc0440c029e51e1cf27d82dc5e435d82829489e3
Athou <jeremiepanzer@gmail.com>
2025-03-02 09:31:27 +0100

    enable quarkus compression (compression in dropwizard was enabled by default)

---
+++ commafeed-server/src/test/java/com/commafeed/integration/CompressionIT.java
@@ -0,0 +1,19 @@
+package com.commafeed.integration;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.google.common.net.HttpHeaders;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+
+@QuarkusTest
+class CompressionIT {
+
+	@ParameterizedTest
+	@ValueSource(strings = { "/rest/server/get", "/openapi.json" })
+	void servedWithCompression(String path) {
+		RestAssured.given().when().get(path).then().statusCode(200).header(HttpHeaders.CONTENT_ENCODING, "gzip");
+	}
+}

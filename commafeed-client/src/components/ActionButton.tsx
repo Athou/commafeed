59927955795e0ab343ac20f4717adf8ae9e74cc9
Athou <jeremiepanzer@gmail.com>
2025-03-02 12:51:26 +0100

    fix tooltips not showing up in mobile view

--- commafeed-client/src/components/ActionButton.tsx
+++ commafeed-client/src/components/ActionButton.tsx
@@ -1,6 +1,6 @@
 import type { MessageDescriptor } from "@lingui/core"
 import { useLingui } from "@lingui/react"
-import { ActionIcon, Button, type ButtonVariant, Tooltip, useMantineTheme } from "@mantine/core"
+import { ActionIcon, Box, Button, type ButtonVariant, Tooltip, useMantineTheme } from "@mantine/core"
 import type { ActionIconVariant } from "@mantine/core/lib/components/ActionIcon/ActionIcon"
 import { Constants } from "app/constants"
 import { useActionButton } from "hooks/useActionButton"
@@ -19,7 +19,7 @@ interface ActionButtonProps {
 /**
  * Switches between Button with label (desktop) and ActionIcon (mobile)
  */
-export const ActionButton = forwardRef<HTMLButtonElement, ActionButtonProps>((props: ActionButtonProps, ref) => {
+export const ActionButton = forwardRef<HTMLDivElement, ActionButtonProps>((props: ActionButtonProps, ref) => {
     const { mobile } = useActionButton()
     const theme = useMantineTheme()
     const { _ } = useLingui()
@@ -27,31 +27,36 @@ export const ActionButton = forwardRef<HTMLButtonElement, ActionButtonProps>((pr
     const label = typeof props.label === "string" ? props.label : props.label && _(props.label)
     const variant = props.variant ?? "subtle"
     const iconOnly = (mobile && !props.showLabelOnMobile) || (!mobile && props.hideLabelOnDesktop)
-    return iconOnly ? (
-        <Tooltip label={label} openDelay={Constants.tooltip.delay}>
-            <ActionIcon
-                ref={ref}
-                color={theme.primaryColor}
-                variant={variant}
-                className={props.className}
-                onClick={props.onClick}
-                aria-label={label}
-            >
-                {props.icon}
-            </ActionIcon>
-        </Tooltip>
-    ) : (
-        <Button
-            ref={ref}
-            variant={variant}
-            size="xs"
-            className={props.className}
-            leftSection={props.icon}
-            onClick={props.onClick}
-            aria-label={label}
-        >
-            {label}
-        </Button>
+
+    return (
+        <Box ref={ref}>
+            {iconOnly && (
+                <Tooltip label={label} openDelay={Constants.tooltip.delay}>
+                    <ActionIcon
+                        color={theme.primaryColor}
+                        variant={variant}
+                        className={props.className}
+                        onClick={props.onClick}
+                        aria-label={label}
+                    >
+                        {props.icon}
+                    </ActionIcon>
+                </Tooltip>
+            )}
+            {!iconOnly && (
+                <Button
+                    variant={variant}
+                    size="xs"
+                    className={props.className}
+                    leftSection={props.icon}
+                    onClick={props.onClick}
+                    aria-label={label}
+                >
+                    {label}
+                </Button>
+            )}
+        </Box>
     )
 })
+
 ActionButton.displayName = "HeaderButton"

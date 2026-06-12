1efd04f6c5a1f5fa8b423c81d5a8caf540e17519
Athou <jeremiepanzer@gmail.com>
2026-06-12 10:10:02 +0200

    fix wrapping of long titles

--- commafeed-client/src/components/content/header/FeedEntryTitle.tsx
+++ commafeed-client/src/components/content/header/FeedEntryTitle.tsx
@@ -1,20 +1,30 @@
 import { Highlight } from "@mantine/core"
 import { useAppSelector } from "@/app/store"
 import type { Entry } from "@/app/types"
+import { tss } from "@/tss"
 
 export interface FeedEntryTitleProps {
     entry: Entry
 }
 
+const useStyles = tss.create(() => ({
+    content: {
+        textWrap: "inherit",
+    },
+}))
+
 export function FeedEntryTitle(props: Readonly<FeedEntryTitleProps>) {
     const search = useAppSelector(state => state.entries.search)
     const keywords = search?.split(" ")
+
+    const { classes } = useStyles()
     return (
         <Highlight
             inherit
             highlight={keywords ?? ""}
             // make sure ellipsis is shown when title is too long
             span
+            className={classes.content}
         >
             {props.entry.title}
         </Highlight>
